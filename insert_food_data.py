77f3bff6e3e8a9f67c544c94ed90393f32f4d724
AtharvaJ10 <atharvajoshi10@gmail.com>
2021-10-28 11:28:37 -0400

    Script to add food data into mongo

---
+++ insert_food_data.py
@@ -0,0 +1,13 @@
+from apps import App
+app = App()
+mongo = app.mongo
+
+f = open('food_data/calories.csv','r')
+l = f.readlines()
+
+for i in range(1,len(l)):
+    l[i] = l[i][1:len(l[i])-2]
+
+for i in range(1,len(l)):
+    temp = l[i].split(",")
+    mongo.db.food.insert({'food': temp[0], 'calories': temp[1]})

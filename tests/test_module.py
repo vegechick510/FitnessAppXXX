e852e1bca19d9a926002562b95de0b27acafcd3b
AtharvaJ10 <atharvajoshi10@gmail.com>
2021-11-02 23:07:53 -0400

    Code coverage and coverage badge

---
+++ tests/test_module.py
@@ -0,0 +1,34 @@
+import unittest
+import application
+
+
+class BasicTestCase(unittest.TestCase):
+
+    def test_home(self):
+        self.app = application.app.test_client()
+        ans =self.app.get('/home')
+        self.assertEqual(ans.status_code,302)
+
+    def test_login(self):
+        self.app = application.app.test_client()
+        ans =self.app.get('/login')
+        self.assertEqual(ans.status_code,200)
+
+    def test_register(self):
+        self.app = application.app.test_client()
+        ans =self.app.get('/register')
+        self.assertEqual(ans.status_code,200)
+
+    def test_logout(self):
+        self.app = application.app.test_client()
+        ans =self.app.get('/logout')
+        self.assertEqual(ans.status_code,200)  
+
+    def test_dashboard(self):
+        self.app = application.app.test_client()
+        ans =self.app.get('/dashboard')
+        self.assertEqual(ans.status_code,200)
+           
+
+if __name__ == '__main__':
+    unittest.main()

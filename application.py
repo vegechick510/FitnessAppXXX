5fabf18716c127edd01e45c8080f8e573b3b656b
AtharvaJ10 <atharvajoshi10@gmail.com>
2021-11-04 17:32:46 -0400

    Fixing travis

--- application.py
+++ application.py
@@ -21,7 +21,7 @@ from flask import json
 
 app = Flask(__name__)
 app.secret_key = 'secret'
-app.config['MONGO_URI'] = 'mongodb://localhost:27017/test?ssl=true&ssl_cert_reqs=CERT_NONE'
+app.config['MONGO_URI'] = 'mongodb://127.0.0.1:27017/test'
 mongo = PyMongo(app)
 
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'

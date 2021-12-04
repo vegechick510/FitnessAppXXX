dca8dee39a12024bc18c5492ad6e53e9a4b3cee5
Sunidhi Hegde <62884191+SunidhiHegde@users.noreply.github.com>
2021-12-04 17:38:32 -0500

    Create forms.py

---
+++ tests/forms.py
@@ -0,0 +1,98 @@
+from datetime import date
+from re import sub
+from flask import app
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.fields.core import DateField, SelectField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from apps import App
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[
+            DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        app_object = App()
+        mongo = app_object.mongo
+
+        temp = mongo.db.user.find_one({'email': email.data}, {'email', 'pwd'})
+        if temp:
+            raise ValidationError('Email already exists!')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
+
+
+class CalorieForm(FlaskForm):
+    app = App()
+    mongo = app.mongo
+
+    cursor = mongo.db.food.find()
+    get_docs = []
+    for record in cursor:
+        get_docs.append(record)
+
+    result = []
+    temp = ""
+    for i in get_docs:
+        temp = i['food'] + ' (' + i['calories'] + ')'
+        result.append((temp, temp))
+
+    food = SelectField(
+        'Select Food', choices=result)
+
+    burnout = StringField('Burn Out', validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+
+class UserProfileForm(FlaskForm):
+    weight = StringField(
+        'Weight', validators=[
+            DataRequired(), Length(
+                min=2, max=20)])
+    height = StringField(
+        'Height', validators=[
+            DataRequired(), Length(
+                min=2, max=20)])
+    goal = StringField(
+        'Goal', validators=[
+            DataRequired(), Length(
+                min=2, max=20)])
+    target_weight = StringField(
+        'Target Weight', validators=[
+            DataRequired(), Length(
+                min=2, max=20)])
+    submit = SubmitField('Save Profile')
+
+
+class HistoryForm(FlaskForm):
+    app = App()
+    mongo = app.mongo
+    date = DateField()
+    submit = SubmitField('Fetch')
+
+
+class EnrollForm(FlaskForm):
+    app = App()
+    mongo = app.mongo
+    submit = SubmitField('Enroll')
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField(
+        'Confirm Password', validators=[
+            DataRequired(), EqualTo('password')])
+    submit = SubmitField('Reset')

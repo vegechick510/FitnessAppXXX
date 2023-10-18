a8f5a682959c15dce5e20c9621f3293b04c7cb9d
Shubham Saboo <sabooshubham2000@gmail.com>
2023-10-18 15:08:10 -0400

    Resolved pylint errors on forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,7 @@
-from datetime import date
-from re import sub
-from flask import app
+# from datetime import date
+# from re import sub
+# from flask import app
+"""Importing modules to create forms"""
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.fields.core import DateField, SelectField
@@ -9,6 +10,7 @@ from apps import App
 
 
 class RegistrationForm(FlaskForm):
+    """Form to collect the registration data of the user"""
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=2, max=20)])
     email = StringField('Email',
@@ -17,22 +19,18 @@ class RegistrationForm(FlaskForm):
     confirm_password = PasswordField(
         'Confirm Password', validators=[
             DataRequired(), EqualTo('password')])
-    
     weight = StringField(
         'Weight', validators=[
             DataRequired(), Length(
                 min=2, max=20)])
-    
     height = StringField(
         'Height', validators=[
             DataRequired(), Length(
                 min=2, max=20)])
-    
     goal = StringField(
         'Goal (Weight Loss/ Muscle Gain)', validators=[
             DataRequired(), Length(
                 min=2, max=20)])
-    
     target_weight = StringField(
         'Target Weight', validators=[
             DataRequired(), Length(
@@ -40,6 +38,7 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_email(self, email):
+        """Function to validate the entered email"""
         app_object = App()
         mongo = app_object.mongo
 
@@ -49,6 +48,7 @@ class RegistrationForm(FlaskForm):
 
 
 class LoginForm(FlaskForm):
+    """Login form to log in to the application"""
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -57,6 +57,7 @@ class LoginForm(FlaskForm):
 
 
 class CalorieForm(FlaskForm):
+    """Form to rcord the calorie intake details of the user"""
     app = App()
     mongo = app.mongo
 
@@ -79,6 +80,7 @@ class CalorieForm(FlaskForm):
 
 
 class UserProfileForm(FlaskForm):
+    """Form to input user details to store their height, weight, goal and target weight"""
     weight = StringField(
         'Weight', validators=[
             DataRequired(), Length(
@@ -99,6 +101,7 @@ class UserProfileForm(FlaskForm):
 
 
 class HistoryForm(FlaskForm):
+    """Form to input the date for which the history needs to be displayed"""
     app = App()
     mongo = app.mongo
     date = DateField()
@@ -106,11 +109,13 @@ class HistoryForm(FlaskForm):
 
 
 class EnrollForm(FlaskForm):
+    """Form to enroll into a particular exercise/event"""
     app = App()
     mongo = app.mongo
     submit = SubmitField('Enroll')
 
 class ResetPasswordForm(FlaskForm):
+    """Form to reset the account password"""
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField(
         'Confirm Password', validators=[
@@ -118,6 +123,7 @@ class ResetPasswordForm(FlaskForm):
     submit = SubmitField('Reset')
 
 class ReviewForm(FlaskForm):
+    """Form to input the different reviews about the application"""
     review = StringField(
         'Review', validators=[
             DataRequired(), Length(

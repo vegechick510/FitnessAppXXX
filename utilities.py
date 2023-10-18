520f1bc05fab07ce358439e58ea5c6b30cb19821
Shubham Saboo <sabooshubham2000@gmail.com>
2023-10-18 15:16:26 -0400

    Resolved pylint errors on utilities.py

--- utilities.py
+++ utilities.py
@@ -1,30 +1,29 @@
+"""Importing modules to check the send_email functions""" 
 import random
-
+import string
 from flask_mail import Message
 from apps import App
-import string
-
 
 class Utilities:
+    """Class to chech the send_email functionality"""
     app = App()
     mail = app.mail
     mongo = app.mongo
-
     def send_email(self, email):
+        """Validates the send email function"""
         msg = Message()
         msg.subject = "BURNOUT - Reset Password Request"
         msg.sender = 'bogusdummy123@gmail.com'
         msg.recipients = [email]
-        random = str(self.get_random_string(8))
-        msg.body = 'Please use the following password to login to your account: ' + random
-        self.mongo.db.ath.update({'email': email}, {'$set': {'temp': random}})
+        random_str = str(self.get_random_string(8))
+        msg.body = 'Please use the following password to login to your account: ' + random_str
+        self.mongo.db.ath.update({'email': email}, {'$set': {'temp': random_str}})
         if self.mail.send(msg):
             return "success"
-        else:
-            return "failed"
+        return "failed"
 
     def get_random_string(self, length):
-        # choose from all lowercase letter
+        """choose from all lowercase letter"""
         letters = string.ascii_lowercase
         result_str = ''.join(random.choice(letters) for i in range(length))
         print("Random string of length", length, "is:", result_str)

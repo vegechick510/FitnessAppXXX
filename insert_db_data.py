adc130090a2b504ecdb0ffab64c075d8ad1a8f8d
Shubham Saboo <sabooshubham2000@gmail.com>
2023-10-18 16:39:07 -0400

    Resolved file error on insert_db_data.py

--- insert_db_data.py
+++ insert_db_data.py
@@ -6,23 +6,17 @@ mongo = app.mongo
 
 def insertfooddata():
     """Inserting the food data from CSV file to MongoDB"""
-<<<<<<< Updated upstream
-    with open("food_data/calories.csv", "r", encoding="ISO-8859-1") as file:
-        f = file.read()
-        l = f.readlines()
-=======
     #with open("food_data/calories.csv", "r", encoding="ISO-8859-1") as file:
     f = open("food_data/calories.csv", "r", encoding="ISO-8859-1")
     l = f.readlines()
->>>>>>> Stashed changes
 
-        for i in range(1, len(l)):
-            l[i] = l[i][1:len(l[i]) - 2]
+    for i in range(1, len(l)):
+        l[i] = l[i][1:len(l[i]) - 2]
 
-        for i in range(1, len(l)):
-            temp = l[i].split(",")
-            mongo.db.food.update_one(
-                {'food': temp[0]}, {'$set': {'calories': temp[1]}}, upsert=True)
+    for i in range(1, len(l)):
+        temp = l[i].split(",")
+        mongo.db.food.update_one(
+            {'food': temp[0]}, {'$set': {'calories': temp[1]}}, upsert=True)
 
 
 def insertexercisedata():

aec1f69225d52bd22644dfbf110e8d16e8c9a948
Shubham Saboo <sabooshubham2000@gmail.com>
2023-10-18 14:56:26 -0400

    Resolved pylint errors in apps.py

--- apps.py
+++ apps.py
@@ -1,9 +1,11 @@
+"""Importing flask to connect to the database"""
 from flask import Flask
 from flask_pymongo import PyMongo
 from flask_mail import Mail
 
 
 class App:
+    """Sending emails to friends"""
     def __init__(self):
         self.app = Flask(__name__)
         self.app.secret_key = 'secret'

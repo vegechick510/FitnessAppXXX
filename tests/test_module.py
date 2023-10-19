4425752d614ff886b88348ff38dc692d6992cc14
RajatChandak2000 <rajatchandak2000@gmail.com>
2023-10-19 19:55:45 -0400

    Adding unit test cases for the redirections of the exercises and for submit_review()

--- tests/test_module.py
+++ tests/test_module.py
@@ -16,45 +16,45 @@ class TestApplication(unittest.TestCase):
 
     def test_home_route(self):
         response = self.app.get('/')
-        self.assertEqual(response.status_code, 302)  # Expect a redirect status code
+        self.assertEqual(response.status_code, 302)  
 
     def test_login_route(self):
         response = self.app.get('/login')
-        self.assertEqual(response.status_code, 200)  # Expect a success status code
+        self.assertEqual(response.status_code, 200)  
 
     def test_register_route(self):
         response = self.app.get('/register')
-        self.assertEqual(response.status_code, 200)  # Expect a success status code
+        self.assertEqual(response.status_code, 200)  
 
     def test_calories_route(self):
-        # Assuming the user is logged in (session is set)
+        
         response = self.app.get('/calories')
-        self.assertEqual(response.status_code, 302)  # Expect a success status code
+        self.assertEqual(response.status_code, 302)  
 
     # def test_display_profile_route(self):
-    #     # Assuming the user is logged in (session is set)
+    #     
     #     with self.app as client:
     #         with client.session_transaction() as sess:
     #             sess['email'] = 'testuser@example.com'
     #         response = client.get('/display_profile')
-    #         self.assertEqual(response.status_code, 200)  # Expect a success status code
+    #         self.assertEqual(response.status_code, 200)  
 
     def test_user_profile_route(self):
-        # Assuming the user is logged in (session is set)
+        
         with self.app as client:
             with client.session_transaction() as sess:
                 sess['email'] = 'testuser@example.com'
             response = client.get('/user_profile')
-            self.assertEqual(response.status_code, 200)  # Expect a success status code
+            self.assertEqual(response.status_code, 200)  
 
     def test_history_route(self):
-        # Assuming the user is logged in (session is set)
+        
         with self.app as client:
             with client.session_transaction() as sess:
                 sess['email'] = 'testuser@example.com'
             response = client.get('/history')
     
-            self.assertEqual(response.status_code, 200)  # Expect a success status code
+            self.assertEqual(response.status_code, 200)  
     def test_bmi_calci_post(self):
         response = self.app.post('/bmi_calc', data={'weight': 70, 'height': 175})
         self.assertEqual(response.status_code, 200)
@@ -84,27 +84,47 @@ class TestApplication(unittest.TestCase):
             self.assertEqual(response.status_code, 200) 
    
     def test_dashboard_route(self):
-    # Assuming the user is logged in (session is set)
+    
         with self.app as client:
             with client.session_transaction() as sess:
                 sess['email'] = 'testuser@example.com'
             response = client.get('/dashboard')
-            self.assertEqual(response.status_code, 200)  # Expect a success status code
+            self.assertEqual(response.status_code, 200)  
 
     def test_add_favorite_route(self):
-        # Assuming the user is logged in (session is set)
+        
         with self.app as client:
             with client.session_transaction() as sess:
                 sess['email'] = 'testuser@example.com'
             response = client.post('/add_favorite', json={'exercise_id': '123', 'action': 'add'})
-            self.assertEqual(response.status_code, 200)  # Expect a success status code
+            self.assertEqual(response.status_code, 200)  
 
     def test_favorites_route(self):
-        # Assuming the user is logged in (session is set)
+        
         with self.app as client:
             with client.session_transaction() as sess:
                 sess['email'] = 'testuser@example.com'
             response = client.get('/favorites')
-            self.assertEqual(response.status_code, 200)  # Expect a success status code
+            self.assertEqual(response.status_code, 200)  
+
+    def test_exercise_routes(self):
+        
+        with self.app as client:
+            with client.session_transaction() as sess:
+                sess['email'] = 'testuser@example.com'
+
+            exercise_routes = ['/yoga', '/swim', '/abbs', '/belly', '/core', '/gym', '/walk', '/dance', '/hrx']
+
+            for route in exercise_routes:
+                response = client.get(route)
+                self.assertEqual(response.status_code, 200)  
+
+    def test_submit_reviews_route(self):
+    
+        with self.app as client:
+            with client.session_transaction() as sess:
+                sess['email'] = 'testuser@example.com'
+            response = client.get('/review')
+            self.assertEqual(response.status_code, 200)  #
 if __name__ == '__main__':
     unittest.main()

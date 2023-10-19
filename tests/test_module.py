edbcca34ae756b801d14981794ff7aa6955d0a96
RajatChandak2000 <rajatchandak2000@gmail.com>
2023-10-19 16:28:25 -0400

    Updating test_module.py.

--- tests/test_module.py
+++ tests/test_module.py
@@ -5,50 +5,50 @@ from application import app
 class BasicTestCase(unittest.TestCase):
 
      def test_logout(self):
-         pass
-    #     self.app = app.test_client()
-    #     ans =self.app.get('/logout')
-    #     self.assertEqual(ans.status_code, 200)
-    #
-    # def test_home(self):
-    #     self.app = app.test_client()
-    #     ans =self.app.get('/home')
-    #     self.assertEqual(ans.status_code, 302)
-    #
-    # def test_login(self):
-    #     self.app = app.test_client()
-    #     ans =self.app.get('/login')
-    #     self.assertEqual(ans.status_code, 200)
-    #
-    # def test_register(self):
-    #     self.app = app.test_client()
-    #     ans =self.app.get('/register')
-    #     self.assertEqual(ans.status_code, 200)
-    #
-    # def test_dashboard(self):
-    #     self.app = app.test_client()
-    #     ans =self.app.get('/dashboard')
-    #     self.assertEqual(ans.status_code, 200)
-    #
-    # def test_friends(self):
-    #     self.app= app.test_client()
-    #     ans=self.app.get('/friends')
-    #     self.assertEqual(ans.status_code, 200)
-    #
-    # def test_calories(self):
-    #     self.app= app.test_client()
-    #     ans=self.app.get('/calories')
-    #     self.assertEqual(ans.status_code, 200)
-    #
-    # def test_user_profile(self):
-    #     self.app= app.test_client()
-    #     ans=self.app.get('/user_profile')
-    #     self.assertEqual(ans.status_code, 200)
-    #
-    # def test_history(self):
-    #     self.app= app.test_client()
-    #     ans=self.app.get('/history')
-    #     self.assertEqual(ans.status_code, 200)
+        pass
+        self.app = app.test_client()
+        ans =self.app.get('/logout')
+        self.assertEqual(ans.status_code, 200)
+    
+     def test_home(self):
+        self.app = app.test_client()
+        ans =self.app.get('/home')
+        self.assertEqual(ans.status_code, 302)
+    
+     def test_login(self):
+        self.app = app.test_client()
+        ans =self.app.get('/login')
+        self.assertEqual(ans.status_code, 200)
+    
+     def test_register(self):
+        self.app = app.test_client()
+        ans =self.app.get('/register')
+        self.assertEqual(ans.status_code, 200)
+    
+     def test_dashboard(self):
+        self.app = app.test_client()
+        ans =self.app.get('/dashboard')
+        self.assertEqual(ans.status_code, 200)
+    
+     def test_friends(self):
+        self.app= app.test_client()
+        ans=self.app.get('/friends')
+        self.assertEqual(ans.status_code, 200)
+    
+     def test_calories(self):
+        self.app= app.test_client()
+        ans=self.app.get('/calories')
+        self.assertEqual(ans.status_code, 200)
+    
+     def test_user_profile(self):
+        self.app= app.test_client()
+        ans=self.app.get('/user_profile')
+        self.assertEqual(ans.status_code, 200)
+    
+     def test_history(self):
+        self.app= app.test_client()
+        ans=self.app.get('/history')
+        self.assertEqual(ans.status_code, 200)
 
 
 if __name__ == '__main__':

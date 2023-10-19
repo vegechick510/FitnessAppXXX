c0710d903794dd283113bad0176a0d45c3845401
Shubham Saboo <sabooshubham2000@gmail.com>
2023-10-19 17:01:21 -0400

    Updated lint.sh

--- lint.sh
+++ lint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-autopep8 --in-place --aggressive insert_food_data.py
+autopep8 --in-place --aggressive insert_db_data.py
 autopep8 --in-place --aggressive application.py
 autopep8 --in-place --aggressive apps.py
 autopep8 --in-place --aggressive utilities.py

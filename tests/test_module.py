c7fc39a2590d5646a81137cee51c28f6eb3e416f
RajatChandak2000 <rajatchandak2000@gmail.com>
2023-10-19 16:43:34 -0400

    Making changes to test_module

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,55 +1,56 @@
 import unittest
 from application import app
+from flask import session
 
+class TestApplication(unittest.TestCase):
 
-class BasicTestCase(unittest.TestCase):
+    def setUp(self):
+        app.testing = True
+        self.app = app.test_client()
 
-     def test_logout(self):
-        pass
-        self.app = app.test_client()
-        ans =self.app.get('/logout')
-        self.assertEqual(ans.status_code, 200)
-    
-     def test_home(self):
-        self.app = app.test_client()
-        ans =self.app.get('/home')
-        self.assertEqual(ans.status_code, 302)
-    
-     def test_login(self):
-        self.app = app.test_client()
-        ans =self.app.get('/login')
-        self.assertEqual(ans.status_code, 200)
-    
-     def test_register(self):
-        self.app = app.test_client()
-        ans =self.app.get('/register')
-        self.assertEqual(ans.status_code, 200)
-    
-     def test_dashboard(self):
-        self.app = app.test_client()
-        ans =self.app.get('/dashboard')
-        self.assertEqual(ans.status_code, 200)
-    
-     def test_friends(self):
-        self.app= app.test_client()
-        ans=self.app.get('/friends')
-        self.assertEqual(ans.status_code, 200)
-    
-     def test_calories(self):
-        self.app= app.test_client()
-        ans=self.app.get('/calories')
-        self.assertEqual(ans.status_code, 200)
-    
-     def test_user_profile(self):
-        self.app= app.test_client()
-        ans=self.app.get('/user_profile')
-        self.assertEqual(ans.status_code, 200)
-    
-     def test_history(self):
-        self.app= app.test_client()
-        ans=self.app.get('/history')
-        self.assertEqual(ans.status_code, 200)
+    def test_home_route(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_code, 302)  # Expect a redirect status code
 
+    def test_login_route(self):
+        response = self.app.get('/login')
+        self.assertEqual(response.status_code, 200)  # Expect a success status code
+
+    def test_register_route(self):
+        response = self.app.get('/register')
+        self.assertEqual(response.status_code, 200)  # Expect a success status code
+
+    def test_calories_route(self):
+        # Assuming the user is logged in (session is set)
+        with self.app as client:
+            with client.session_transaction() as sess:
+                sess['email'] = 'testuser@example.com'
+            response = client.get('/calories')
+            self.assertEqual(response.status_code, 200)  # Expect a success status code
+
+    def test_display_profile_route(self):
+        # Assuming the user is logged in (session is set)
+        with self.app as client:
+            with client.session_transaction() as sess:
+                sess['email'] = 'testuser@example.com'
+            response = client.get('/display_profile')
+            self.assertEqual(response.status_code, 200)  # Expect a success status code
+
+    def test_user_profile_route(self):
+        # Assuming the user is logged in (session is set)
+        with self.app as client:
+            with client.session_transaction() as sess:
+                sess['email'] = 'testuser@example.com'
+            response = client.get('/user_profile')
+            self.assertEqual(response.status_code, 200)  # Expect a success status code
+
+    def test_history_route(self):
+        # Assuming the user is logged in (session is set)
+        with self.app as client:
+            with client.session_transaction() as sess:
+                sess['email'] = 'testuser@example.com'
+            response = client.get('/history')
+            self.assertEqual(response.status_code, 200)  # Expect a success status code
 
 if __name__ == '__main__':
     unittest.main()
